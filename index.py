ff8a72d52fecfe93da98ebe91f9ca21738f08129
Ethel0213 <kayhozkun@gmail.com>
2023-10-25 15:01:39 +0800

    make it better

--- index.py
+++ index.py
@@ -35,8 +35,8 @@ def account():
     else:
         return render_template("account.html")
 
-if __name__ == "__main__":
+#if __name__ == "__main__":
 
-	app.run()
+	#app.run()
 
-	serve(app, host='0.0.0.0', port=8080)
+	#serve(app, host='0.0.0.0', port=8080)
